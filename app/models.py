076d4842a0ee06b17fb7adf69b2d225de255bf34
sami-mai <samirah.maison@gmail.com>
2018-04-08 20:42:04 +0300

    define model class news

---
+++ app/models.py
@@ -0,0 +1,14 @@
+class News:
+
+    '''
+    News class to define News Object
+    '''
+
+    def __init__(self, source.name, author, title, description, url, urlToImage, publishedAt):
+        self.source.name = sourceName
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
