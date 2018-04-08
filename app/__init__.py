1db5d46e483071e6d088d20c9f1cef77242a578c
sami-mai <samirah.maison@gmail.com>
2018-04-08 20:19:53 +0300

    create app configurations

---
+++ app/__init__.py
@@ -0,0 +1,20 @@
+from flask import Flask
+
+from config import config_options
+
+
+
+
+def create_app(config_name):
+
+    app = Flask(__name__)
+
+    # Creating the app configurations
+
+    app.config.from_object(config_options[config_name])
+    config_options[config_name].init_app(app)
+
+
+
+
+    
