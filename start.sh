6f7718e4a0f84d5694b08cd9af5016b654089dca
sami-mai <samirah.maison@gmail.com>
2018-04-09 00:03:10 +0300

    setup environment variables

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-export NEWS_API_KEY = '8614d42799304d6b82b3a0fc2c624fc5'
-export SECRET_KEY = 'sami-mai-news'
+export NEWS_API_KEY=8614d42799304d6b82b3a0fc2c624fc5
+export SECRET_KEY=sami-mai-news
 
 python3.6 manage.py server
